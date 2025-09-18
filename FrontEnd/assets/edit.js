edf0f4c6e3f7ce15b0c7fe7c103546773360e629
Patrick <abadie.patrick27@gmail.com>
2025-09-18 14:26:46 +0200

    commencement du carre 2

--- FrontEnd/assets/edit.js
+++ FrontEnd/assets/edit.js
@@ -8,6 +8,10 @@ modifcard.addEventListener("click", function() {
     const backgroundOverlay = document.createElement("div");
     backgroundOverlay.classList.add("backgroundOverlay");
     document.body.appendChild(backgroundOverlay);
+    backgroundOverlay.addEventListener("click", function() {
+        document.body.removeChild(carre);
+        document.body.removeChild(backgroundOverlay);
+    })
 
     const closeButton = document.createElement("span");
     closeButton.classList.add("closebtn");
@@ -27,6 +31,11 @@ modifcard.addEventListener("click", function() {
     galleryContainer.classList.add("galleryContainer");
     carre.appendChild(galleryContainer);
 
+    const line = document.createElement("hr");
+    carre.appendChild(line);
+    
+
+    
     const mainGallery = document.querySelector(".gallery");
 
     stockworks.forEach((work) => {
@@ -58,6 +67,67 @@ modifcard.addEventListener("click", function() {
                 }
             });
         });
+    });
+
+    const addPhotoBtn = document.createElement("button");
+    addPhotoBtn.classList.add("btnadd");
+    addPhotoBtn.textContent = "Ajouter une photo";
+    carre.appendChild(addPhotoBtn);
+    addPhotoBtn.addEventListener("click", function() {
+        document.body.removeChild(carre);
+        const carre2 = document.createElement("div");
+        carre2.classList.add("carre");
+        document.body.appendChild(carre2);
+
+        backgroundOverlay.addEventListener("click", function() {
+            document.body.removeChild(carre2);
+            document.body.removeChild(backgroundOverlay);
+        })
+    
+
+        carre2.appendChild(closeButton);
+        closeButton.addEventListener("click", function() {
+            document.body.removeChild(carre2);
+            document.body.removeChild(backgroundOverlay);
+        });
+
+        const backButton = document.createElement("span");
+        backButton.classList.add("arrow-back");
+        backButton.innerHTML = '<i class="fa-solid fa-arrow-left"></i>';
+        carre2.appendChild(backButton);
+        backButton.addEventListener("click", function() {
+            document.body.removeChild(carre2);
+            document.body.appendChild(carre);
+        });
+
+        const title = document.createElement("h2");
+        title.textContent = "Ajout photo";
+        title.classList.add("carretitle");
+        carre2.appendChild(title)
+
+        const imgadd = document.createElement("div");
+        imgadd.classList.add("carreimgadd");
+        carre2.appendChild(imgadd);
+
+        const pic = document.createElement("label");
+        pic.innerHTML = '<i class="fa-regular fa-image"></i>';
+        pic.classList.add("pic");
+        imgadd.appendChild(pic);
+        
+        const addimg = document.createElement("span");
+        addimg.classList.add("addimg");
+        addimg.textContent = "+ Ajouter photo";
+        imgadd.appendChild(addimg);
+
+            
+        const infoText = document.createElement("p");
+        infoText.textContent = "jpg, png : 4mo max";
+        imgadd.appendChild(infoText);
+
+
+
+        const line = document.createElement("hr");
+        carre2.appendChild(line);
     });
 });
 
